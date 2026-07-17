b78036a8ffbc71e1b5de570d73ca822e3841345b
Strawberry <naz_strawberry@gmail.com>
2026-07-17 18:06:58 +0330

    Add encryption flow to CLI
    and Improve encryption CLI user guidance

--- src/cli.py
+++ src/cli.py
@@ -16,6 +16,60 @@ def get_menu_choice():
     return input("\nSelect an option: ").strip()
 
 
+from src.file_encryptor import (
+    generate_key,
+    encrypt_file,
+)
+
+from src.key_manager import save_key
+
+
+def encrypt_file_flow():
+
+    print("\n=== Encrypt File ===")
+
+    print("\nEnter the path of the file you want to encrypt.")
+    print("Example: C:\\Users\\Name\\Desktop\\secret.txt")
+
+    input_path = input("\nInput file path: ").strip()
+
+
+    print("\nEnter the output encrypted file name.")
+    print("Example: secret.enc")
+
+    output_path = input("\nEncrypted file path: ").strip()
+
+    key = generate_key()
+
+    encrypt_file(
+        input_path,
+        output_path,
+        key,
+    )
+
+    save = input(
+    "\nDo you want to save the encryption key? (Y/N): "
+).strip().upper()
+    if save == "Y":
+
+        print("\nThe key file is required later to decrypt your file.")
+        print("Example: secret.key")
+
+        key_path = input(
+            "\nEncryption key file path: "
+        ).strip()
+
+        save_key(
+            key,
+            key_path,
+        )
+
+        print("\nEncryption key saved.")
+
+    print("\nFile encrypted successfully.")
+
+
+
 def run():
 
     while True:
@@ -29,7 +83,7 @@ def run():
             break
 
         elif choice == "1":
-            print("Encrypt File selected.")
+            encrypt_file_flow()
 
         elif choice == "2":
             print("Decrypt File selected.")
